59353dd364f5f49880397521a0f152f77c30d577
Jacob Sharp <Jacob@Jacobs-Work-Comp.local>
2015-10-26 08:08:05 -0400

    did some minor stylizing, got the album functions completely working.

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var albumTemplate = _.template($('#albumTemp').html());
 var picturesTemplate = _.template($('#picTemp').html());
+var albumListTemp = _.template($('#asideTemp').html());
 var coverPhoto;
+var pint = 1;
+var linc = 1;
 var inc = 1;
 var int = 1;
 var className;
+_.each(albums, function(item, idx, arr){
+  $('aside').append(albumListTemp(item));
+  $('aside').addClass('invisible');
+  className = 'li-displayed-' + linc;
+  if(idx === linc-1){
+    $('.albumLiItem:nth-child(' + linc + ')').addClass(className);
+    linc++;
+  }
+});
 _.each(albums, function(item, idx, arr){
   _.each(item.album, function(item){
     albumPhoto = item.photo;
@@ -16,9 +28,15 @@ _.each(albums, function(item, idx, arr){
   $('section').append(albumTemplate(item));
     if(idx === inc-1){
       className = "displayed-" + inc;
+
       _.each(item.album, function(item){
-      $('.pictures-grid:nth-child(' + int + ')').addClass(className);
-      int++;
+        $('.pictures-grid:nth-child(' + int + ')').addClass(className);
+        for(var i = 0; i < albums[0].album.length; i++){
+          $('.pictureDisplay:eq(' + (pint-1) + ')').addClass('pic' + i);
+          $('.pictureTitle:eq(' + (pint-1) + ')').addClass('pic' + i);
+          pint++;
+        }
+        int++;
     });
     inc++;
   }
@@ -29,12 +47,56 @@ var albumPhoto;
 var photoName;
 $('.albumsDisplay').on('click', function(){
   for(var i = 0; i < albums.length; i++){
-    console.log(i);
+    $('h1').addClass('invisible');
+    $('aside').removeClass('invisible');
+    $('.return').addClass('invisible');
     if($(this).attr('class') === $('.albumsDisplay.displayed-' + i + '').attr('class')){
-      console.log("worked");
-      console.log('true');
-      $('.pictures-grid.displayed-' + (i+1) + '').addClass('displayed');
+      $('section').append('<h4>' + albums[i].name + '</h4>');
+      $('.pictures-grid.displayed-' + (i+1)).addClass('displayed');
       $('.grid-albums').addClass('invisible');
     }
+  }
+});
+$('li').on('click', function(){
+  for(var i = 0; i < albums.length; i++){
+    if($(this).attr('class') === $('.albumLiItem.li-displayed-' + (i+1)).attr('class')){
+      $('h4').html(albums[i].name);
+      for(var j = 0; j <albums.length; j++){
+        $('.pictures-grid.displayed-' + (j+1)).removeClass('displayed');
+      }
+      $('.pictures-grid.displayed-' + (i+1)).addClass('displayed');
+      $('.pictureDisplay').removeClass('invisible');
+      $('.pictureDisplay').removeClass('enlargedPicture');
+    }
+  }
+});
+$('.pictureDisplay').on('click', function(){
+  for(var i = 0; i < albums[i].album.length;i++){
+    if($(this).attr('class') === $('.pictureDisplay.pic' + i).attr('class')){
+      $('.pictureDisplay.pic' + i).addClass('enlargedPicture');
+      $('.pictureTitle.pic' + i).css('left', '80px');
+      $('.albumLiItem').addClass('invisible');
+      $('.return').removeClass('invisible');
+      $('h4').addClass('invisible');
+      $('aside').addClass('invisible');
+    }
+    else{
+        $('.pictureDisplay.pic' + i).addClass('invisible');
+        $('.pictureTitle.pic' + i).addClass('invisible');
+        // $('.return:eq('+i+')').removeClass('invisible');
+    }
+  }
+});
+$('.return').on('click', function(){
+  $('.return').removeClass('invisible');
+  if($(this).attr('class') === $('.return').attr('class')){
+    $('.pictureTitle').css('left', '0px');
+    $('.pictureDisplay').removeClass('enlargedPicture');
+    $('.pictureDisplay').removeClass('invisible');
+    $('.albumLiItem').removeClass('invisible');
+    $('.pictureTitle').removeClass('invisible');
+    $('.return').addClass('invisible');
+    $('h4').removeClass('invisible');
+    $('aside').removeClass('invisible');
   }
 });
